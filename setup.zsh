7d9783896849fec352c0a0e5ad354f2f1244c04d
Daniel <edblancas@gmail.com>
2016-04-18 19:05:28 -0500

    Add alias ls to gls, and modules intit updgrade

--- setup.zsh
+++ setup.zsh
@@ -2,7 +2,7 @@ echo "--> Symlinking..." < `tty` > `tty`
 homesick link dotfiles-castle < `tty` > `tty`
 
 echo "-- Submodules init..." < `tty` > `tty`
-git submodule init < `tty` > `tty`
+git submodule update --init --recursive < `tty` > `tty`
 
 echo "--> Installing nvim plugins with vundle..." < `tty` > `tty`
 nvim +PluginInstall! +PluginClean +qall < `tty` > `tty`
