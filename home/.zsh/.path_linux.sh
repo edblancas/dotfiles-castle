ccd2fa4bf8160bf311cef423e5705819caa9fc14
Daniel Blancas <edblancas@gmail.com>
2017-07-04 12:48:59 -0500

    Add path for local compiled vim

--- home/.zsh/.path_linux.sh
+++ home/.zsh/.path_linux.sh
@@ -2,4 +2,4 @@
 export M2_HOME="/opt/apache-maven-3.5.0"
 export M2="$M2_HOME/bin"
 export JAVA_HOME="/opt/jdk1.8.0_131"
-export PATH="/home/linuxbrew/.linuxbrew/bin:$JAVA_HOME/bin:$M2:$PATH"
+export PATH="$HOME/local/bin:/home/linuxbrew/.linuxbrew/bin:$JAVA_HOME/bin:$M2:$PATH"
