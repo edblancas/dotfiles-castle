d10d6729ec1bbba9be0118c2d04e6d9defacf89f
Daniel Blancas <edblancas@gmail.com>
2017-08-25 00:05:14 -0500

    Change path to java 8 and fix tmux bind-key vim

--- home/.zsh/.path_macOS.sh
+++ home/.zsh/.path_macOS.sh
@@ -1,5 +1,5 @@
 # PATH
-export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.7.0_79.jdk/Contents/Home"
+export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.8.0_102.jdk/Contents/Home"
 # Maven 3.2.5, last to support Java 6
 export M2_HOME="$HOME/opt/apache-maven-3.2.5"
 export M2="$M2_HOME/bin"
