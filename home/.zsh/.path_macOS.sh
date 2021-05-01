79cb12b6868cd3b69a963b07a68e24d9de46d5e2
Daniel Blancas <edblancas@gmail.com>
2021-04-30 19:06:04 -0500

    fix paths mac

--- home/.zsh/.path_macOS.sh
+++ home/.zsh/.path_macOS.sh
@@ -1,38 +1,21 @@
 # PATH
 
-# Java Home default to version 8 at the begining. Use this cause it can differ from
-# one mac to another (rappi, personal, 13"). And cause the 1.8 is the more used.
-export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
-# Maven 3.2.5, last to support Java 6
-export M2_HOME="$SDKMAN_DIR/candidates/maven/current"
-export M2="$M2_HOME/bin"
+# Java Home default to version 11
+export JAVA_HOME=`/usr/libexec/java_home -v 11`
+
 # local apps installed like vim from github
 export HOME_LOCAL="$HOME/local"
+
 # Homebrew binaries, the paths are not exclusive of homebrew!
 export HOMEBREW="/usr/local"
 export HOMEBREW_COREUTILS="/usr/local/opt/coreutils/libexec"
 
-# yarn via script
-export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
-
 export PATH="$JAVA_HOME/bin:$M2:$HOMEBREW_COREUTILS/gnubin:$HOMEBREW/bin:$HOMEBREW/sbin:/opt/local/bin:/opt/local/sbin:$PATH"
 
-# rustup
-export PATH="$HOME/.cargo/bin:$PATH"
-
 export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:/opt/local/share/man:$MANPATH"
-
-export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
 
 # BIN HOME
 export PATH="$HOME/bin:$PATH"
 
-export GOPATH="$HOME/opt/go"
-export PATH="$PATH:$GOPATH/bin:$HOME/.cargo/bin"
-
-export SDKMAN_DIR="$HOME/.sdkman"
-
-export NVM_DIR="$HOME/.nvm"
-
 # Add user base binary directory for python (pipenv, pep8, ...)
-export PATH="$HOME/Library/Python/3.7/bin:$PATH"
+export PATH="$HOME/Library/Python/3.9/bin:$PATH"
