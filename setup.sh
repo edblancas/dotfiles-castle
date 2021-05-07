4d68611e1412f58e4f897e8a9f15c690143e4e61
Daniel Blancas <edblancas@gmail.com>
2021-05-06 20:55:31 -0500

    rm spaceship prompt

--- setup.sh
+++ setup.sh
@@ -5,11 +5,8 @@ echo "-- Submodules init..." < `tty` > `tty`
 git submodule update --init --recursive < `tty` > `tty`
 
 echo "-- Symlink pure prompt..." < `tty` > `tty`
-ln -s $HOME/.zfunction/pure/pure.zsh /usr/local/share/zsh/site-functions/prompt_pure_setup < `tty` > `tty`
-ln -s $HOME/.zfunctions/pure/async.zsh /usr/local/share/zsh/site-functions/async < `tty` > `tty`
-
-echo "-- Symlink spaceship prompt..." < `tty` > `tty`
-ln -s "$HOME/.config/oh-my-zsh/custom/themes/spaceship-prompt/spaceship.zsh-theme" "$HOME/.config/oh-my-zsh/custom/themes/spaceship.zsh-theme" < `tty` > `tty`
+ln -s $HOME/.config/oh-my-zsh/custom/themes/pure/pure.zsh /usr/local/share/zsh/site-functions/prompt_pure_setup < `tty` > `tty`
+ln -s $HOME/.config/oh-my-zsh/custom/themes/pure/async.zsh /usr/local/share/zsh/site-functions/async < `tty` > `tty`
 
 echo "--> Installing nvim plugins with vundle..." < `tty` > `tty`
 nvim +PluginInstall! +PluginClean +qall < `tty` > `tty`
