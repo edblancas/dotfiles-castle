686e6c61b7d6c08412ac12da9a6be54a7c4d0fef
Daniel Blancas <edblancas@gmail.com>
2022-10-30 14:15:48 -0600

    wip

--- setup.sh
+++ setup.sh
@@ -7,15 +7,6 @@ git submodule update --init --recursive < `tty` > `tty`
 echo "--> Symlink iTerm2 settings (only if its clean installation)..." < `tty` > `tty`
 ln -s $HOME/.config/iterm/com.googlecode.iterm2.plist $HOME/Library/Preferences/com.googlecode.iterm2.plist < `tty` > `tty`
 
-echo "--> Installing nvim plugins with Plug..." < `tty` > `tty`
-nvim +PluginInstall! +PluginClean +qall < `tty` > `tty`
-
-echo "--> Install Coc Plugins in vim..." < `tty` > `tty`
-nvim -c ':CocInstall coc-diagnostic | quit' < `tty` > `tty`
-nvim -c ':CocInstall coc-snippets | quit' < `tty` > `tty`
-
-echo "--> Done! Happy Vimming! :x" < `tty` > `tty`
-
 echo "--> Symlink corgi-emacs..." < `tty` > `tty`
 ln -s $HOME/.config/corgi-emacs/sample-config $HOME/.emacs.d < `tty` > `tty`
 
