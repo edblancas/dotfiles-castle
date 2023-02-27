e05c7690b8a115418c1a2403cfc874c5cdaf63a3
Daniel Blancas <edblancas@gmail.com>
2023-02-27 12:41:04 -0300

    add alacritty config bassed in the github tread https://github.com/alacritty/alacritty/issues/862\#issuecomment-633445473

--- home/.hammerspoon/init.lua
+++ home/.hammerspoon/init.lua
@@ -1,8 +1,11 @@
 hs.hotkey.bind({"command"}, "f12", function()
-  local alacritty = hs.application.find('alacritty')
-  if alacritty:isFrontmost() then
+  local alacritty = hs.application.get('Alacritty')
+  if (alacritty ~= nil and alacritty:isFrontmost()) then
     alacritty:hide()
   else
     hs.application.launchOrFocus("/Applications/Alacritty.app")
+    local alacritty = hs.application.find('alacritty')
+    alacritty.setFrontmost(alacritty)
+    alacritty.activate(alacritty)
   end
 end)
