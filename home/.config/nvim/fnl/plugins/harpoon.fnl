a9027026b01dab722dc5c037dc15140b09c835be
Daniel Blancas <edblancas@gmail.com>
2024-08-08 01:51:39 -0600

    disable some mac kitty shortcuts and add harpoon nvim
    disable cmd+w soemtimes pressed by accident

---
+++ home/.config/nvim/fnl/plugins/harpoon.fnl
@@ -0,0 +1,18 @@
+[{1 :ThePrimeagen/harpoon
+  :branch :harpoon2
+  :dependencies ["nvim-lua/plenary.nvim"]
+  :config (fn []
+    (let [h (require :harpoon)
+          lst (h:list)]
+      (h:setup)
+      (vim.keymap.set "n" "<leader>ba" (fn [] (lst:add)))
+      (vim.keymap.set "n" "<D-e>" (fn [] (h.ui:toggle_quick_menu (h:list))))
+
+      (vim.keymap.set "n" "<M-D-1>" (fn [] (lst:select 1)))
+      (vim.keymap.set "n" "<M-D-2>" (fn [] (lst:select 2)))
+      (vim.keymap.set "n" "<M-D-3>" (fn [] (lst:select 3)))
+      (vim.keymap.set "n" "<M-D-4>" (fn [] (lst:select 4)))
+
+      ;Toggle previous & next buffers stored within Harpoon list
+      (vim.keymap.set "n" "<leader>bp" (fn [] (lst:prev)))
+      (vim.keymap.set "n" "<leader>bn" (fn [] (lst:next)))))}]
---
+++ home/.config/nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,39 @@
+-- [nfnl] Compiled from fnl/plugins/harpoon.fnl by https://github.com/Olical/nfnl, do not edit.
+local function _1_()
+  local h = require("harpoon")
+  local lst = h:list()
+  h:setup()
+  local function _2_()
+    return lst:add()
+  end
+  vim.keymap.set("n", "<leader>ba", _2_)
+  local function _3_()
+    return h.ui:toggle_quick_menu(h:list())
+  end
+  vim.keymap.set("n", "<D-e>", _3_)
+  local function _4_()
+    return lst:select(1)
+  end
+  vim.keymap.set("n", "<M-D-1>", _4_)
+  local function _5_()
+    return lst:select(2)
+  end
+  vim.keymap.set("n", "<M-D-2>", _5_)
+  local function _6_()
+    return lst:select(3)
+  end
+  vim.keymap.set("n", "<M-D-3>", _6_)
+  local function _7_()
+    return lst:select(4)
+  end
+  vim.keymap.set("n", "<M-D-4>", _7_)
+  local function _8_()
+    return lst:prev()
+  end
+  vim.keymap.set("n", "<leader>bp", _8_)
+  local function _9_()
+    return lst:next()
+  end
+  return vim.keymap.set("n", "<leader>bn", _9_)
+end
+return {{"ThePrimeagen/harpoon", branch = "harpoon2", dependencies = {"nvim-lua/plenary.nvim"}, config = _1_}}
