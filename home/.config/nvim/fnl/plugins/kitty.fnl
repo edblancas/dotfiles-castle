312f5554e3a21e96f331f9e3a02b68f08ac77d1b
Daniel Blancas <edblancas@gmail.com>
2024-09-26 22:04:12 -0600

    c-h/j/k/l now works in normal an insert mode

--- home/.config/nvim/fnl/plugins/kitty.fnl
+++ home/.config/nvim/fnl/plugins/kitty.fnl
@@ -16,9 +16,9 @@
  {1 :MunsMan/kitty-navigator.nvim
     :config (fn []
               (let [kn (require :kitty-navigator)]
-                	(vim.keymap.set :n :<C-j> kn.navigateDown {:silent false})
-                	(vim.keymap.set :n :<C-k> kn.navigateUp {:silent false})
-                	(vim.keymap.set :n :<C-l> kn.navigateRight {:silent false})
-                  (vim.keymap.set :n :<C-h> kn.navigateLeft {:silent false})))
+                	(vim.keymap.set [:n :i] :<C-j> kn.navigateDown {:silent false})
+                	(vim.keymap.set [:n :i] :<C-k> kn.navigateUp {:silent false})
+                	(vim.keymap.set [:n :i] :<C-l> kn.navigateRight {:silent false})
+                  (vim.keymap.set [:n :i] :<C-h> kn.navigateLeft {:silent false})))
     :build ["cp navigate_kitty.py ~/.config/kitty"
             "cp pass_keys.py ~/.config/kitty"]}]
--- home/.config/nvim/lua/plugins/kitty.lua
+++ home/.config/nvim/lua/plugins/kitty.lua
@@ -10,9 +10,9 @@ local function _2_()
 end
 local function _3_()
   local kn = require("kitty-navigator")
-  vim.keymap.set("n", "<C-j>", kn.navigateDown, {silent = false})
-  vim.keymap.set("n", "<C-k>", kn.navigateUp, {silent = false})
-  vim.keymap.set("n", "<C-l>", kn.navigateRight, {silent = false})
-  return vim.keymap.set("n", "<C-h>", kn.navigateLeft, {silent = false})
+  vim.keymap.set({"n", "i"}, "<C-j>", kn.navigateDown, {silent = false})
+  vim.keymap.set({"n", "i"}, "<C-k>", kn.navigateUp, {silent = false})
+  vim.keymap.set({"n", "i"}, "<C-l>", kn.navigateRight, {silent = false})
+  return vim.keymap.set({"n", "i"}, "<C-h>", kn.navigateLeft, {silent = false})
 end
 return {{"mikesmithgh/kitty-scrollback.nvim", enabled = true, lazy = true, cmd = {"KittyScrollbackGenerateKittens", "KittyScrollbackCheckHealth"}, event = {"User KittyScrollbackLaunch"}, version = "*", config = _1_}, {"edblancas/kitty-runner.nvim", branch = "fix", config = _2_}, {"MunsMan/kitty-navigator.nvim", config = _3_, build = {"cp navigate_kitty.py ~/.config/kitty", "cp pass_keys.py ~/.config/kitty"}}}
