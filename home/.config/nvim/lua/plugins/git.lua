4cfaeef30fef48f04abff07b8ec51ee11edb4b05
Daniel Blancas <edblancas@gmail.com>
2024-10-05 17:27:19 -0600

    iupdate

--- home/.config/nvim/lua/plugins/git.lua
+++ home/.config/nvim/lua/plugins/git.lua
@@ -2,4 +2,4 @@
 local function _1_()
   return vim.keymap.set({"n", "i"}, "<D-0>", "<cmd>G<CR>")
 end
-return {{"lewis6991/gitsigns.nvim", config = true}, {"kdheepak/lazygit.nvim", cmd = {"LazyGit", "LazyGitConfig", "LazyGitCurrentFile", "LazyGitFilter", "LazyGitFilterCurrentFile"}, dependencies = {"nvim-lua/plenary.nvim"}, keys = {{"<leader>lg", "<cmd>LazyGit<cr>", mode = {"i", "n"}, desc = "LazyGit"}}}, {"tpope/vim-fugitive", config = _1_}}
+return {{"lewis6991/gitsigns.nvim", config = true}, {"kdheepak/lazygit.nvim", cmd = {"LazyGit", "LazyGitConfig", "LazyGitCurrentFile", "LazyGitFilter", "LazyGitFilterCurrentFile"}, dependencies = {"nvim-lua/plenary.nvim"}, keys = {{"<D-9>", "<cmd>LazyGit<cr>", mode = {"i", "n"}, desc = "LazyGit"}}}, {"tpope/vim-fugitive", config = _1_}}
