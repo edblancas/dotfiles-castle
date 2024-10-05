408276f233db5bdb8e72e9c9463bd2d029a7a009
Daniel Blancas <edblancas@gmail.com>
2024-10-04 23:49:57 -0600

    more idea like keybinding

--- home/.config/nvim/fnl/plugins/git.fnl
+++ home/.config/nvim/fnl/plugins/git.fnl
@@ -10,6 +10,8 @@
     :dependencies ["nvim-lua/plenary.nvim"]
     ;setting the keybinding for LazyGit with 'keys' is recommended in
     ;order to load the plugin when the command is run for the first time
-    :keys [{1 "<leader>lg" 2 "<cmd>LazyGit<cr>" :desc "LazyGit"}]}
- {1 :tpope/vim-fugitive}]
+    :keys [{1 "<D-9>" 2 "<cmd>LazyGit<cr>" :mode [:i :n] :desc "LazyGit"}]}
+ {1 :tpope/vim-fugitive
+    :config (fn []
+              (vim.keymap.set [:n :i] "<D-0>" "<cmd>G<CR>"))}]
 
--- home/.config/nvim/lua/plugins/git.lua
+++ home/.config/nvim/lua/plugins/git.lua
@@ -1,2 +1,5 @@
 -- [nfnl] Compiled from fnl/plugins/git.fnl by https://github.com/Olical/nfnl, do not edit.
-return {{"lewis6991/gitsigns.nvim", config = true}, {"kdheepak/lazygit.nvim", cmd = {"LazyGit", "LazyGitConfig", "LazyGitCurrentFile", "LazyGitFilter", "LazyGitFilterCurrentFile"}, dependencies = {"nvim-lua/plenary.nvim"}, keys = {{"<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit"}}}, {"tpope/vim-fugitive"}}
+local function _1_()
+  return vim.keymap.set({"n", "i"}, "<D-0>", "<cmd>G<CR>")
+end
+return {{"lewis6991/gitsigns.nvim", config = true}, {"kdheepak/lazygit.nvim", cmd = {"LazyGit", "LazyGitConfig", "LazyGitCurrentFile", "LazyGitFilter", "LazyGitFilterCurrentFile"}, dependencies = {"nvim-lua/plenary.nvim"}, keys = {{"<D-9>", "<cmd>LazyGit<cr>", mode = {"i", "n"}, desc = "LazyGit"}}}, {"tpope/vim-fugitive", config = _1_}}
