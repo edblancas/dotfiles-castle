1e071c67f3f30b835027a028db860dc8cc4769ed
Daniel Blancas <edblancas@gmail.com>
2024-10-08 07:32:13 -0600

    vim

--- home/.config/nvim/fnl/plugins/bookmarks.fnl
+++ home/.config/nvim/fnl/plugins/bookmarks.fnl
@@ -1,13 +1,6 @@
-[
- {1 :crusj/bookmarks.nvim
- :branch :main
- :dependencies [:nvim-tree/nvim-web-devicons]
- :config (fn []
-   (let [bk (require :bookmarks)
-         tl (require :telescope)]
-     (bk.setup {:keymap {:toggle "<D-2>"
-                         :add "<F3>"}})
-     (tl.load_extension :bookmarks)
-     (vim.keymap.set [:n :i] "<D-2>" "<cmd>lua require('bookmarks').toggle_bookmarks()<cr>")
-     (vim.keymap.set [:n :i] "<F3>" "<cmd>lua require('bookmarks').add_bookmarks()<cr>")))}
- ]
+[{1 :crusj/bookmarks.nvim
+    :branch :main
+    :dependencies [:nvim-tree/nvim-web-devicons]
+    :keys [{1 "<D-2>" 2 "<cmd>lua require('bookmarks').toggle_bookmarks()<cr>" :mode [:n :i]}
+          {1 "<F3>" 2 "<cmd>lua require('bookmarks').add_bookmarks()<cr>" :mode [:n :i]}]
+    :config true}]
--- home/.config/nvim/lua/plugins/bookmarks.lua
+++ home/.config/nvim/lua/plugins/bookmarks.lua
@@ -1,10 +1,2 @@
 -- [nfnl] Compiled from fnl/plugins/bookmarks.fnl by https://github.com/Olical/nfnl, do not edit.
-local function _1_()
-  local bk = require("bookmarks")
-  local tl = require("telescope")
-  bk.setup({keymap = {toggle = "<D-2>", add = "<F3>"}})
-  tl.load_extension("bookmarks")
-  vim.keymap.set({"n", "i"}, "<D-2>", "<cmd>lua require('bookmarks').toggle_bookmarks()<cr>")
-  return vim.keymap.set({"n", "i"}, "<F3>", "<cmd>lua require('bookmarks').add_bookmarks()<cr>")
-end
-return {{"crusj/bookmarks.nvim", branch = "main", dependencies = {"nvim-tree/nvim-web-devicons"}, config = _1_}}
+return {{"crusj/bookmarks.nvim", branch = "main", dependencies = {"nvim-tree/nvim-web-devicons"}, keys = {{"<D-2>", "<cmd>lua require('bookmarks').toggle_bookmarks()<cr>", mode = {"n", "i"}}, {"<F3>", "<cmd>lua require('bookmarks').add_bookmarks()<cr>", mode = {"n", "i"}}}, config = true}}
