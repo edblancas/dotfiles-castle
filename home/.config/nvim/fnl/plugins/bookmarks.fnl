987f11dea4bac47e3ad579c565ba264c18b10fa5
Daniel Blancas <edblancas@gmail.com>
2024-10-11 11:33:02 -0600

    change float bookmars keybind

--- home/.config/nvim/fnl/plugins/bookmarks.fnl
+++ home/.config/nvim/fnl/plugins/bookmarks.fnl
@@ -1,6 +1,6 @@
 [{1 :crusj/bookmarks.nvim
     :branch :main
     :dependencies [:nvim-tree/nvim-web-devicons]
-    :keys [{1 "<D-2>" 2 "<cmd>lua require('bookmarks').toggle_bookmarks()<cr>" :mode [:n :i]}
+    :keys [{1 "<D-F3>" 2 "<cmd>lua require('bookmarks').toggle_bookmarks()<cr>" :mode [:n :i]}
           {1 "<F3>" 2 "<cmd>lua require('bookmarks').add_bookmarks()<cr>" :mode [:n :i]}]
     :config true}]
--- home/.config/nvim/lua/plugins/bookmarks.lua
+++ home/.config/nvim/lua/plugins/bookmarks.lua
@@ -1,2 +1,2 @@
 -- [nfnl] Compiled from fnl/plugins/bookmarks.fnl by https://github.com/Olical/nfnl, do not edit.
-return {{"crusj/bookmarks.nvim", branch = "main", dependencies = {"nvim-tree/nvim-web-devicons"}, keys = {{"<D-2>", "<cmd>lua require('bookmarks').toggle_bookmarks()<cr>", mode = {"n", "i"}}, {"<F3>", "<cmd>lua require('bookmarks').add_bookmarks()<cr>", mode = {"n", "i"}}}, config = true}}
+return {{"crusj/bookmarks.nvim", branch = "main", dependencies = {"nvim-tree/nvim-web-devicons"}, keys = {{"<D-F3>", "<cmd>lua require('bookmarks').toggle_bookmarks()<cr>", mode = {"n", "i"}}, {"<F3>", "<cmd>lua require('bookmarks').add_bookmarks()<cr>", mode = {"n", "i"}}}, config = true}}
