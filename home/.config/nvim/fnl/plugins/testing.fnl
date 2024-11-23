70d15b6bbcc72c09dff08b4acaa09ce4254a790f
Daniel Blancas <edblancas@gmail.com>
2024-11-23 16:55:48 -0600

    add vim-test, easy way to test than neo-test

--- home/.config/nvim/fnl/plugins/testing.fnl
+++ home/.config/nvim/fnl/plugins/testing.fnl
@@ -4,14 +4,25 @@
                    :antoinemadec/FixCursorHold.nvim
                    :nvim-treesitter/nvim-treesitter
                    ;python runner 
-                   :nvim-neotest/neotest-python]
+                   :nvim-neotest/neotest-python
+                   :nvim-neotest/neotest-jest]
     :config (fn []
               (let [nt (require :neotest)]
-                (nt.setup {:adapters [(require :neotest-python)]})))
+                (nt.setup {:adapters [(require :neotest-python)
+                                      (require :neotest-jest)]})))
     :keys [{1 "<leader>rt" 2 ":lua require('neotest').run.run()<cr>" :desc "Run nearest test"}
            {1 "<leader>rf" 2 ":lua require('neotest').run.run()<cr>" :desc "Run current test file"}
            {1 "<F17>" 2 ":Neotest summary<cr>" :desc "Display test summary"}
            {1 "<C-F17>" 2 ":Neotest output-panel<cr>" :desc "Display test output-panel"}
            {1 "<C-F16>" 2 ":lua require('neotest').watch.toggle()<cr>" :desc "Watch test file"}
            {1 "<leader>rw" 2 ":lua require('neotest').watch.toggle()<cr>" :desc "Watch test file"}
-           {1 "<leader>rx" 2 "<cmd>Neotest stop<cr>" :desc "Stop test"} ]}]
+           {1 "<leader>rx" 2 "<cmd>Neotest stop<cr>" :desc "Stop test"}]}
+
+ {1 :vim-test/vim-test
+    :config (fn []
+              (vim.cmd "let test#strategy = 'kitty'"))
+    :keys [{1 "<leader>tb" 2 ":TestFile<cr>" :desc "Run current test file"}
+           {1 "<leader>tn" 2 ":TestNearest<cr>" :desc "Run nearest test"}
+           {1 "<leader>ts" 2 ":TestSuite<cr>" :desc "Run all tests"}
+           {1 "<leader>tl" 2 ":TestLast<cr>" :desc "Run last test"}
+           {1 "<leader>tt" 2 ":TestVisit<cr>" :desc "Run test under cursor"}]}]
--- home/.config/nvim/lua/plugins/testing.lua
+++ home/.config/nvim/lua/plugins/testing.lua
@@ -1,6 +1,9 @@
 -- [nfnl] Compiled from fnl/plugins/testing.fnl by https://github.com/Olical/nfnl, do not edit.
 local function _1_()
   local nt = require("neotest")
-  return nt.setup({adapters = {require("neotest-python")}})
+  return nt.setup({adapters = {require("neotest-python"), require("neotest-jest")}})
 end
-return {{"nvim-neotest/neotest", dependencies = {"nvim-neotest/nvim-nio", "nvim-lua/plenary.nvim", "antoinemadec/FixCursorHold.nvim", "nvim-treesitter/nvim-treesitter", "nvim-neotest/neotest-python"}, config = _1_, keys = {{"<leader>rt", ":lua require('neotest').run.run()<cr>", desc = "Run nearest test"}, {"<leader>rf", ":lua require('neotest').run.run()<cr>", desc = "Run current test file"}, {"<F17>", ":Neotest summary<cr>", desc = "Display test summary"}, {"<C-F17>", ":Neotest output-panel<cr>", desc = "Display test output-panel"}, {"<C-F16>", ":lua require('neotest').watch.toggle()<cr>", desc = "Watch test file"}, {"<leader>rw", ":lua require('neotest').watch.toggle()<cr>", desc = "Watch test file"}, {"<leader>rx", "<cmd>Neotest stop<cr>", desc = "Stop test"}}}}
+local function _2_()
+  return vim.cmd("let test#strategy = 'kitty'")
+end
+return {{"nvim-neotest/neotest", dependencies = {"nvim-neotest/nvim-nio", "nvim-lua/plenary.nvim", "antoinemadec/FixCursorHold.nvim", "nvim-treesitter/nvim-treesitter", "nvim-neotest/neotest-python", "nvim-neotest/neotest-jest"}, config = _1_, keys = {{"<leader>rt", ":lua require('neotest').run.run()<cr>", desc = "Run nearest test"}, {"<leader>rf", ":lua require('neotest').run.run()<cr>", desc = "Run current test file"}, {"<F17>", ":Neotest summary<cr>", desc = "Display test summary"}, {"<C-F17>", ":Neotest output-panel<cr>", desc = "Display test output-panel"}, {"<C-F16>", ":lua require('neotest').watch.toggle()<cr>", desc = "Watch test file"}, {"<leader>rw", ":lua require('neotest').watch.toggle()<cr>", desc = "Watch test file"}, {"<leader>rx", "<cmd>Neotest stop<cr>", desc = "Stop test"}}}, {"vim-test/vim-test", config = _2_, keys = {{"<leader>tb", ":TestFile<cr>", desc = "Run current test file"}, {"<leader>tn", ":TestNearest<cr>", desc = "Run nearest test"}, {"<leader>ts", ":TestSuite<cr>", desc = "Run all tests"}, {"<leader>tl", ":TestLast<cr>", desc = "Run last test"}, {"<leader>tt", ":TestVisit<cr>", desc = "Run test under cursor"}}}}
