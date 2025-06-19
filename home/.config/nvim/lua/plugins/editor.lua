2af96445e1d4db8284f65f8e74f1035aa5fd9639
Daniel Blancas <edblancas@gmail.com>
2025-06-19 07:17:15 -0600

    feat(nvim): typo

--- home/.config/nvim/lua/plugins/editor.lua
+++ home/.config/nvim/lua/plugins/editor.lua
@@ -2,17 +2,21 @@ return {
   {
     'lukas-reineke/indent-blankline.nvim',
     main = 'ibl',
-    opts = {indent = {char = "▏"},
-             scope = {char = "▏",
-                     show_start = false,
-                    show_end = false}}
+    opts = {
+      indent = { char = "▏" },
+      scope = {
+        char = "▏",
+        show_start = false,
+        show_end = false
+      }
+    }
   },
   {
     "mbbill/undotree",
     keys = {
       { 
         "<leader>u", 
-        "<cmd>UndotreeToggle
-    },
-}
+        "<cmd>UndotreeToggle<CR>"
+    }
+  }
 }
