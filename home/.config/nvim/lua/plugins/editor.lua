50c95dd2c783664aae53777890d0367b4d51e8a6
Daniel Blancas <edblancas@gmail.com>
2025-06-19 07:13:40 -0600

    feat(nvim): add undotree plugin

--- home/.config/nvim/lua/plugins/editor.lua
+++ home/.config/nvim/lua/plugins/editor.lua
@@ -1,9 +1,18 @@
 return {
-  {'lukas-reineke/indent-blankline.nvim',
+  {
+    'lukas-reineke/indent-blankline.nvim',
     main = 'ibl',
     opts = {indent = {char = "▏"},
              scope = {char = "▏",
                      show_start = false,
                     show_end = false}}
-  }
+  },
+  {
+    "mbbill/undotree",
+    keys = {
+      { 
+        "<leader>u", 
+        "<cmd>UndotreeToggle
+    },
+}
 }
